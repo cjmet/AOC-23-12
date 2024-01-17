1ec339f72b73ee322871fbc87e6cfb31ccbd4424
CJ Metcalfe <khaaiev@gmail.com>
2024-01-17 16:44:33 -0500

    Add project files.

---
+++ ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿
+namespace AOC_23_12
+{
+	public static class ExtensionMethods
+	{
+		public static string FindFileInVisualStudio(string filename, SearchOption searchOption = SearchOption.AllDirectories)
+		{
+			string pushd = Directory.GetCurrentDirectory();
+			string[] files = Directory.GetFiles(Directory.GetCurrentDirectory(), filename, searchOption);
+			if (files.Length == 0)
+			{
+				Directory.SetCurrentDirectory("..");
+				// string value = directory.FindFileInVisualStudio(directory, filename, SearchOption.TopDirectoryOnly);
+				string value = FindFileInVisualStudio(filename, SearchOption.TopDirectoryOnly);
+				Directory.SetCurrentDirectory(pushd);
+				return value;
+			}
+			else
+			{
+				Directory.SetCurrentDirectory(pushd);
+				return files[0];
+			}
+		}
+		
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using static AOC_23_12.ExtensionMethods;
+
+int sum = 0;
+var filename = FindFileInVisualStudio("input.txt");
+filename =  FindFileInVisualStudio("test.txt");
+Console.WriteLine($"Attempting to read {filename}");
+Console.WriteLine();
+foreach (string line in File.ReadLines(filename))
+{
+	Console.Write($"{line} => ");
+	int combinations = GetCombinations(line);
+	Console.WriteLine(combinations);
+	sum += combinations;
+}
+
+
+Console.WriteLine();
+Console.WriteLine($"Sum: {sum}");
+Debug.Assert(sum == 54597 || sum == 54504);
+Environment.Exit(0);
+// </main>
+
+
+/* *********************************************************************** */
+
+int GetCombinations(string line)
+{
+	return 0;
+}
+
+
+
+
+
