eb958610dda224ddf66656d4374b40e4529b5e58
CJ Metcalfe <khaaiev@gmail.com>
2024-01-19 16:26:18 -0500

    240119 - Success

--- Program.cs
+++ Program.cs
@@ -5,15 +5,17 @@ using System.Diagnostics;
 using System.Runtime.CompilerServices;
 
 int sum = 0;
+int springArraySize = 0;
+int dataArraySize = 0;
 
 var filename = FindFileInVisualStudio("input.txt");
-filename = FindFileInVisualStudio("test.txt");
+// filename = FindFileInVisualStudio("test.txt");
 Console.WriteLine($"Attempting to read {filename}");
 Console.WriteLine();
 
 foreach (string line in File.ReadLines(filename))
 {
-	Console.WriteLine($"{line} ");
+	Console.Write($"{line} => ");
 	if (line.Length == 0)
 	{
 		Console.WriteLine("Blank Line Found, Exiting.");
@@ -25,19 +27,25 @@ foreach (string line in File.ReadLines(filename))
 	Assert(parts.Length == 2, "Error[0030] Invalid Input");
 	string springsString = parts[0];
 	string dataString = parts[1];
+	springArraySize = int.Max(springArraySize, springsString.Length);
+	dataArraySize = int.Max(dataArraySize, dataString.Length);
 
 	Springs springs = new Springs(springsString, dataString);
-	int combinations = springs.FindClusters(0,0);
 
-	Console.WriteLine($"Combinations: {combinations}");
-	Console.WriteLine();
+
+	int combinations = springs.FindRecursiveMatches();				// < ============================
+
+
+	Console.WriteLine($"{combinations}");
 	sum += combinations;
 }
 
 
+Console.WriteLine();
 Console.WriteLine("===========================================================");
+Console.WriteLine($"Spring Array Size: {springArraySize},     Data Array Size: {dataArraySize}");
+Springs info = new Springs();
+Console.WriteLine($"Usage: {info.Usage},     Cache Used: {info.CacheUsed}");
 Console.WriteLine($"Sum: {sum}");
-// Assert(sum == 54597 || sum == 54504);
+Assert(sum == 7032, "Incorrect Answer");
 Environment.Exit(0);
-// </main>
-
--- Springs.cs
+++ Springs.cs
@@ -1,4 +1,5 @@
-﻿using static AOC_23_12.Extensions;
+﻿using System.Diagnostics;
+using static AOC_23_12.Extensions;
 
 namespace AOC_23_12
 {
@@ -6,139 +7,110 @@ namespace AOC_23_12
 	{
 		char[] _springs;
 		int[] _data;
+		static int[,] _cache;
+		static int _cacheUsed = 0;
+		static int _usage = 0;
 
+        public int CacheUsed { get { return _cacheUsed; } }
+		public int Usage { get { return _usage; } }
+
+		public Springs() => new Springs(".", "1"); 
 		public Springs(string springsString, string dataString)
 		{
-			_springs = springsString.ToCharArray();
+			
+			_springs = OptimizeDots(springsString).ToCharArray();
 			_data = dataString.Split(",").Select(x => int.Parse(x)).ToArray();
+			_cache = new int[25,15];
+			foreach (int i in _cache) _cache[i % 25, i / 25] = -1;
+		}
+
+		private string OptimizeDots(string source)
+		{
+			string optimized = "";
+			foreach (char c in source)
+			{
+				if (optimized.Length < 1 || optimized.Last() != '.' || c != '.') optimized += c;
+			}
+			return optimized;
+		}
+
+		public int FindRecursiveMatches() => FindRecursiveMatches(0, 0);
+		public int FindRecursiveMatches(int springIndex, int clusterIndex)
+		{
+			_usage++;
+			if (_cache[springIndex, clusterIndex] > 0) { _cacheUsed++; return _cache[springIndex, clusterIndex]; }
+
+			if (clusterIndex >= _data.Length)		
+				if (TailCheck(springIndex)) return _cache[springIndex, clusterIndex] = 1;  
+				else return _cache[springIndex, clusterIndex] = 0;
+
+			if (IsEOL(springIndex)) return _cache[springIndex, clusterIndex] = 0;
+			int found = 0;
+
+			if (IsSpring(springIndex)) found += FindRecursiveMatches(springIndex + 1, clusterIndex);  // Increment Right
+			if (SpringsMatchesCluster(springIndex, _data[clusterIndex])) found += FindRecursiveMatches(springIndex + _data[clusterIndex] + 1, clusterIndex + 1); // Increment Right Searching for Cluster
+
+			return _cache[springIndex, clusterIndex] = found;
 		}
 
 
-		public int Zero() { return 0; }
 
 
-		//public int GetCombinations(int springIndex = 0, int clusterIndex = 0)
-		//{
-		//	Assert(springIndex >= 0 && clusterIndex >= 0);
-		//	if (springIndex >= _springs.Length || clusterIndex >= _data.Length) return 0;
+		// Match One Cluster
+		bool SpringsMatchesCluster(int springIndex, int cluster)
+		{
 
-		//	return 1;
-		//}
+			if (!IsSpringOrEOL(springIndex - 1)) return false;
+
+			// Check the Cluster
+			for (int i = 0; i < cluster; i++)
+			{
+				if (!IsDamaged(springIndex + i)) return false;
+			}
+
+			if (!IsSpringOrEOL(springIndex + cluster)) return false;
+
+			return true;
+		}
+
 
 
 		// ************************************************************************************************
 
 
-		public int FindClusters(int springIndex, int clusterIndex)
+
+		bool TailCheck(int springIndex)		// aka: no '#' to the right of the answer, invalidating that answer as premature
 		{
-			int found = 0;
-			do
-			{
-				int cluster = _data[clusterIndex];
-				Debut.WriteLine($"Spring {springIndex} Cluster {clusterIndex} {cluster}");
-				int index = FindFirstCluster(springIndex, cluster);  // cjm
-
-				// if the clusters sequence is not complete increment clusterIndex and springIndex, and continue	
-				// index + cluster + 1 to chomp the tail character
-				// if (index >= 0 && clusterIndex < _data.Length - 1) FindClusters(index + cluster + 1, clusterIndex + 1);
-
-				// if the sequence is complete and has a valid tail, increment found and continue
-				if (index >= 0 && clusterIndex == _data.Length - 1 && TailCheck(index + cluster + 1))
-				{
-					found++;
-					if (!IsSpring(springIndex)) return found;
-				}
-			}
-			while (++springIndex < _springs.Length);
-
-			return found;
-		}
-
-
-
-		// Increment Right Searching for Cluster, fail if not spring or EOL
-		int FindFirstCluster(int springIndex, int cluster)
-		{
-			do
-			{
-				if (SpringsMatchesCluster(springIndex, cluster) >= 0)
-				{
-					return springIndex;
-				}
-				if (!IsSpring(++springIndex)) return -1;
-			} while (springIndex < _springs.Length);
-			return -1;
-		}
-
-
-
-		// Match One Cluster
-		int SpringsMatchesCluster(int springIndex, int cluster)
-		{
-			int j = 0;
-
-			if (springIndex + cluster > _springs.Length)
-			{
-				return -1;
-			}
-			// Check the Cluster
-			for (j = 0; j < cluster; j++)
-			{
-				if (!IsDamaged(springIndex + j))
-				{
-					return -1;
-				}
-			}
-			// Check the Tail
-			if (IsSpringOrEOL(springIndex + j))
-			{
-				return springIndex;
-			}
-
-			return -1;
-		}
-
-
-
-		bool TailCheck(int springIndex)
-		{
-			while (springIndex < _springs.Length)
-			{
-				if (!IsSpringOrEOL(springIndex)) return false;
-				else springIndex++;
-			}
+			if (IsEOL(springIndex)) return true;
+			for (int i = springIndex; i < _springs.Length; i++) if (!IsSpring(i)) return false;
 			return true;
 		}
 
 		bool IsEOL(int springIndex)
 		{
 			if ((springIndex) >= _springs.Length) return true;
-			else return false;
+			if ((springIndex) < 0) return true;
+			return false;
 		}
 
-		bool IsSpring(int springIndex)
+		bool IsSpring(int springIndex)          // '.', or '?'
 		{
 			if (IsEOL(springIndex)) return false;
-			else if (_springs[springIndex] == '.' || _springs[springIndex] == '?') return true;
-			else return false;
+			if (_springs[springIndex] == '.' || _springs[springIndex] == '?') return true;
+			return false;
 		}
 
-		bool IsSpringOrEOL(int springIndex)
+		bool IsSpringOrEOL(int springIndex)     // '.', '?', or EOL
 		{
-			if (IsSpring(springIndex) || IsEOL(springIndex)) return true;
-			else return false;
+			if (IsEOL(springIndex) || IsSpring(springIndex)) return true;
+			return false;
 		}
 
-		bool IsDamaged(int springIndex)
+		bool IsDamaged(int springIndex)         // '#', or '?'
 		{
+			if (IsEOL(springIndex)) return false;
 			if (_springs[springIndex] == '#' || _springs[springIndex] == '?') return true;
-			else return false;
-		}
-
-		bool IsDamagedOrEOL(int springIndex)
-		{
-			if (IsDamaged(springIndex) || IsEOL(springIndex)) return true;
-			else return false;
+			return false;
 		}
 
 	}
