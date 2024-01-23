316682dd0abff38da3f370d1d375b4e7ce044c91
Christian J Metcalfe <khaaiev@gmail.com>
2024-01-23 17:46:37 -0500

    Update 240123a

---
+++ FileEngine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AOC_23_12.Extensions;
+
+namespace AOC_23_12
+{
+    public class FileEngine
+    {
+
+        public FileEngine(string filename, Part part)
+        {
+            long sum = 0;
+            int linenum = 0;
+            Springs.Reset();
+
+            filename = FindFileInVisualStudio(filename);
+            Debug.WriteLine($"Attempting to read {filename}");
+
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            foreach (string line in File.ReadLines(filename))
+            {
+                linenum++;
+                Debug.Write($"{linenum,4}: {line} => ");
+                if (line.Length == 0)
+                {
+                    Console.WriteLine("Blank Line Found, Exiting.");
+                    break;
+                }
+
+                // Split the line into its parts
+                string[] parts = line.Split(" ");
+                Assert(parts.Length == 2, "Error[0030] Invalid Input");
+                string springsString = parts[0];
+                string dataString = parts[1];
+
+                // Part 2
+                if (part == Part.Part2)
+                {
+                    string tmpSpring = springsString;
+                    string tmpData = dataString;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        springsString += "?" + tmpSpring;
+                        dataString += "," + tmpData;
+                    }
+                }
+
+                Springs springs = new Springs(springsString, dataString);
+                long combinations = springs.FindRecursiveMatches();              // < ============================
+
+                Debug.WriteLine($"{combinations}");
+                sum += combinations;
+            }
+            stopWatch.Stop();
+
+            Springs info = new Springs();
+            Console.WriteLine($"Filename: {filename},      Lines: {linenum},    Part: {part}");
+            Console.WriteLine($"Sum: {sum,13},     Elapsed Time: {stopWatch.Elapsed}s,     Operations: {info.Usage,6},     Cache Used: {info.CacheUsed}");
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,116 +1,64 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AOC_23_12;
 using System.Diagnostics;
+using System.IO.Enumeration;
 using static AOC_23_12.Extensions;
+using System.Threading.Tasks;
 
-class Program
+namespace AOC_23_12
 {
-    enum Part { Part1, Part2 };     // Top level Statements means this must at the bottom of the file.
-
-    static void Main(string[] args)
+    public enum Part { Part1, Part2 };     // Top level Statements means this must at the bottom of the file.
+    class Program
     {
 
-        // Print_Test_Array("??.??.??.??.??.", "1,1,1,1,1");
-        // Print_Test_Array(".#.????.???.#.??.", "1,1,1,1,1");
-        Springs.UseCache = false;
-        Test_Array("???#???.??????????????#???.??????????????#???.??????????????#???.??????????????#???.???????????", "6,6,2,6,6,2,6,6,2,6,6,2,6,6,2", false);
-        Springs.UseCache = true;
-        Test_Array("???#???.??????????????#???.??????????????#???.??????????????#???.??????????????#???.???????????", "6,6,2,6,6,2,6,6,2,6,6,2,6,6,2", false);
-        Console.WriteLine();
-
-        Springs.UseCache = false;
-        SubMain("test.txt", Part.Part1);
-
-        SubMain("input.txt", Part.Part1);
-
-        SubMain("test.txt", Part.Part2);
-
-        //Task.Run(() => SubMain("test.txt", Part.Part2)).Wait(TimeSpan.FromSeconds(3));
-        //var action = new Action(() => SubMain("test.txt", Part.Part2));
-        //var task = new Task(action);
-        //task.Run.Wait(TimeSpan.FromSeconds(3));
-        //if (task.IsCanceled) Console.WriteLine("Operation Timed Out");
-        
-        Springs.UseCache = true;
-        SubMain("test.txt", Part.Part2);
-        // SubMain("input.txt", Part.Part2);
-
-        Environment.Exit(0);
-
-
-
-        ////**********************************************************************************************************************
-
-
-
-        void SubMain(string filename, Part part)
+        static void Main(string[] args)
         {
-            long sum = 0;
-            Springs.Reset();
 
-            filename = FindFileInVisualStudio(filename);
-            Debug.WriteLine($"Attempting to read {filename}");
+            // Print_Test_Array("??.??.??.??.??.", "1,1,1,1,1");
+            // Print_Test_Array(".#.????.???.#.??.", "1,1,1,1,1");
+            Springs.UseCache = false;
+            Test_Array("???#???.??????????????#???.??????????????#???.??????????????#???.??????????????#???.???????????", "6,6,2,6,6,2,6,6,2,6,6,2,6,6,2", false);
+            Springs.UseCache = true;
+            Test_Array("???#???.??????????????#???.??????????????#???.??????????????#???.??????????????#???.???????????", "6,6,2,6,6,2,6,6,2,6,6,2,6,6,2", false);
+            Console.WriteLine();
 
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            foreach (string line in File.ReadLines(filename))
+            Springs.UseCache = false;
+            new FileEngine("test.txt", Part.Part1);
+
+            new FileEngine("input.txt", Part.Part1);
+
+            new FileEngine("test.txt", Part.Part2);
+
+            Springs.UseCache = true;
+            new FileEngine("input.txt", Part.Part2);
+
+            Environment.Exit(0);
+
+
+
+            // *********************************************************
+
+
+            void Test_Array(string springString, string dataString, bool printCache)
             {
-                Debug.Write($"{line} => ");
-                if (line.Length == 0)
-                {
-                    Console.WriteLine("Blank Line Found, Exiting.");
-                    break;
-                }
 
-                // Split the line into its parts
-                string[] parts = line.Split(" ");
-                Assert(parts.Length == 2, "Error[0030] Invalid Input");
-                string springsString = parts[0];
-                string dataString = parts[1];
 
-                // Part 2
-                if (part == Part.Part2)
-                {
-                    string tmpSpring = springsString;
-                    string tmpData = dataString;
-                    for (int i = 0; i < 4; i++)
-                    {
-                        springsString += "?" + tmpSpring;
-                        dataString += "," + tmpData;
-                    }
-                }
-
-                Springs springs = new Springs(springsString, dataString);
+                Springs springs = new Springs(springString, dataString);
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
                 long combinations = springs.FindRecursiveMatches();              // < ============================
-
-                Debug.WriteLine($"{combinations}");
-                sum += combinations;
+                stopWatch.Stop();
+                if (printCache)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    springs.PrintCache();
+                }
+                Console.WriteLine($"UseCache: {Springs.UseCache,5}     Combinations: {combinations},     Elapsed Time: {stopWatch.Elapsed}s");
             }
-            stopWatch.Stop();
-
-            Springs info = new Springs();
-            Console.WriteLine($"Sum: {sum,13},     Elapsed Time: {stopWatch.Elapsed}s,     Operations: {info.Usage,6},     Cache Used: {info.CacheUsed}");
-
         }
 
 
 
-        void Test_Array(string springString, string dataString, bool printCache)
-        {
-
-
-            Springs springs = new Springs(springString, dataString);
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
-            long combinations = springs.FindRecursiveMatches();              // < ============================
-            stopWatch.Stop();
-            if (printCache)
-            {
-                Console.WriteLine();
-                Console.WriteLine();
-                springs.PrintCache();
-            }
-            Console.WriteLine($"UseCache: {Springs.UseCache,5}     Combinations: {combinations},     Elapsed Time: {stopWatch.Elapsed}s");
-        }
     }
 }
